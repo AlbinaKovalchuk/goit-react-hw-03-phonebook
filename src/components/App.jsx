e049607d7cbf890da88634e604655ebdd71d9bbe
Albina Kovalchuk <alkadjin78@i.ua>
2023-04-06 01:40:22 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,12 @@ export const App = () => {
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center',
+        // alignItems: 'center',
         fontSize: 40,
         color: '#010101'
       }}
     >
-      React homework template
+      hw-03-phonebook
     </div>
   );
 };
